52f77baccd6d64c39bf8e45447578790cfb6c193
Jordi Salvans <jsalvans@viewnext.com>
2018-02-22 18:11:34 +0100

    a

--- ejercicioEclipse/src/ejercicioEclipse/Main.java
+++ ejercicioEclipse/src/ejercicioEclipse/Main.java
@@ -2,7 +2,7 @@ package ejercicioEclipse;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		System.out.println("Hola. Que tal?");
 		System.out.println("¿Me has escuchado?");
 		System.out.println("Muy bien");
