5db9e6f989613a8c223d6b6b2f700d0cac656cdc
Jordi Salvans <jsalvans@viewnext.com>
2018-02-22 18:08:44 +0100

    Commit merged

--- ejercicioEclipse/src/ejercicioEclipse/Main.java
+++ ejercicioEclipse/src/ejercicioEclipse/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola. Que tal?");
+		System.out.println("¿Me has escuchado?");
 		System.out.println("Muy bien");
 	}
 
